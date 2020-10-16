a0db4848a0cd72fdf2fa9c67023a8dad0b9c8b49
hideunderbush <jialhuang8-c@my.cityu.edu.hk>
2020-10-16 08:18:09 +0800

    rm unnecessary scripts

--- projector_factor.sh
+++
@@ -1,2 +0,0 @@
-python3 projector_factor.py --ckpt checkpoint/anime_freezeFC_G1-4_22000.pt --model checkpoint/network-snapshot-metfaces2.pt --size 256 --size2 1024 --fact factors/factor_anime_FC.pt /home/chloe/workspace1/stylegan2_mapping/face2anime6_b_origin2.png 
-
